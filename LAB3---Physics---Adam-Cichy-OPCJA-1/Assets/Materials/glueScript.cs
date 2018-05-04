0aa2005317c2122d6e07b66d7de1d24318ef9a1d
Adam Cichy <adam.cichyy3@gmail.com>
2018-05-04 12:11:07 +0200

    Skończony projekt

---
+++ LAB3---Physics---Adam-Cichy-OPCJA-1/Assets/Materials/glueScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class glueScript : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 0, 15) * Time.deltaTime);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		collision.gameObject.GetComponent<Rigidbody>().useGravity = false;
+		collision.gameObject.GetComponent<Rigidbody>().freezeRotation = true;
+		collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+		collision.gameObject.GetComponent<Collider>().enabled = false;
+		collision.transform.localRotation=Quaternion.identity;
+		collision.gameObject.transform.parent = transform;
+
+		//collision.transform.localPosition=Vector3.zero;
+
+/*
+		float x = collision.gameObject.transform.parent.localScale.x;
+		float y = collision.gameObject.transform.parent.localScale.y;
+		float z = collision.gameObject.transform.parent.localScale.z;
+*/
+
+		/*
+		collision.transform.localScale = new Vector3 (x / collision.gameObject.transform.parent.localScale.x,
+		                                             y / collision.gameObject.transform.parent.localScale.y,
+		                                             z / collision.gameObject.transform.parent.localScale.z);
+*/
+		collision.transform.localScale = new Vector3 ( .02f,.02f,0.02f);
+
+	}
+}
